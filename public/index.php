06ce4bdc710e91145a8f09aebe180110f6c38d54
youssef moustafa <youssef.mostafa11111111@gmail.com>
2026-05-04 01:48:28 +0300

    frontend_work

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
